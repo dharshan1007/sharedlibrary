1f7415bdc01d7b97d99949e42bfb4a22e942f649
dharshan1007 <81615110+dharshan1007@users.noreply.github.com>
2022-10-11 17:57:45 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -3,12 +3,3 @@ def call(name)
   echo "hey, ${name}, How are you?"
 }
 
-def add(x,y)
-{
-echo "addition of ${x} and ${y} is ${x+y}
-}
-
-def mul(x,y)
-{
-echo "multiplication of ${x} and ${y} is ${x*y}
-}
