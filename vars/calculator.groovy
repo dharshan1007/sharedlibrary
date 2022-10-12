1bea04b64e46b9d9e9b350cc3edda70d41458243
dharshan1007 <81615110+dharshan1007@users.noreply.github.com>
2022-10-12 11:01:52 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,9 +1,9 @@
 def add(x,y)
 {
-echo "addition of ${x} and ${y} is ${x+y}
+echo "addition of ${x} and ${y} is ${x+y}"
 }
 
 def mul(x,y)
 {
-echo "multiplication of ${x} and ${y} is ${x*y}
+echo "multiplication of ${x} and ${y} is ${x*y}"
 }
